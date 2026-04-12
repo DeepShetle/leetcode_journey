37fe23633def34e78260c4341c63df4a617cedea
DeepShetle <nhatmcyt@gmail.com>
2026-04-12 20:59:59 +0700

    Time: 0 ms (100%), Space: 10.9 MB (30.26%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,17 @@
+int majorityElement(int* nums, int numsSize){
+    int candidate;
+    int count = 0;
+    for(int i = 0; i < numsSize; i++){
+        if(count == 0){
+            candidate = nums[i];
+            count = 1;
+        }
+        else if(nums[i] == candidate){
+            count += 1;
+        }
+        else if(nums[i] != candidate){
+            count -= 1;
+        }
+    }
+    return candidate;
+}
